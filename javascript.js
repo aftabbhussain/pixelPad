e31a3c4367e0fa94aad63475b23970dbea3a9eb2
aftabbhussain <88221488+aftabbhussain@users.noreply.github.com>
2025-06-28 21:46:05 +0530

    Add the sketching functionality
    Finally added the javascript code to make the sketchpad work as intended

--- javascript.js
+++ javascript.js
@@ -1,13 +1,36 @@
-let s = 9;
-let cellSide = Math.floor(720/s);
-const container = document.querySelector("#container");
-let width = cellSide*s + 'px';
-container.style.width = width;
-container.style.height = width;
-for(let i = 1; i <= s*s; i++){
-    const div = document.createElement("div");
-    div.style.width = cellSide + 'px';
-    div.style.height = cellSide + 'px';
-    container.appendChild(div);
+function constructGrid(s){
+    const main = document.querySelector(".main")
+    const container = document.createElement("div");
+    container.id = "container";
+    main.appendChild(container);
+    let cellSide = Math.floor(720/s);
+    let width = cellSide*s + 'px';
+    container.style.width = width;
+    container.style.height = width;
+    for(let i = 1; i <= s*s; i++){
+        const div = document.createElement("div");
+        div.style.width = cellSide + 'px';
+        div.style.height = cellSide + 'px';
+        container.appendChild(div);
+    }
+
+    container.addEventListener("mouseover", (e) => {
+        if(e.target !== container) {
+            e.target.style.backgroundColor = "black";
+        }
+    });
 }
 
+const applybtn = document.querySelector("#applybtn");
+applybtn.addEventListener("click", () =>{
+    container.remove();
+    const gridSize = document.querySelector("#grid-size").value;
+    constructGrid(gridSize);
+});
+
+const resetbtn = document.querySelector("#resetbtn");
+resetbtn.addEventListener("click", () => {
+    container.remove();
+    const gridSize = document.querySelector("#grid-size").value;
+    constructGrid(gridSize);
+})
